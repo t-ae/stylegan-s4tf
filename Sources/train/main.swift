ab409663578d48a7b1107b598e94fa69fbf25eb7
t-ae <araki.t.ae@outlook.com>
2019-08-31 21:45:21 +0900

    Scalarized

--- Sources/train/main.swift
+++ Sources/train/main.swift
@@ -133,10 +133,10 @@ for step in 1... {
 
     print("step: \(step), alpha: \(generator.alpha), g: \(lossG), d: \(lossD)")
     
-    writer.addScalar(tag: "lv\(level)/lossG", scalar: lossG.scalar!, globalStep: step)
-    writer.addScalar(tag: "lv\(level)/lossD", scalar: lossD.scalar!, globalStep: step)
+    writer.addScalar(tag: "lv\(level)/lossG", scalar: lossG.scalarized(), globalStep: step)
+    writer.addScalar(tag: "lv\(level)/lossD", scalar: lossD.scalarized(), globalStep: step)
     if Config.loss == .lsgan {
-        writer.addScalar(tag: "lv\(level)/dout_mean", scalar: discriminator.outputMean.value.scalar!, globalStep: step)
+        writer.addScalar(tag: "lv\(level)/dout_mean", scalar: discriminator.outputMean.value.scalarized(), globalStep: step)
     }
     
     imageCount += minibatchSize
